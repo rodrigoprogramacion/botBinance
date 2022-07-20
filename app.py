cad22ed3b7f0807ae7e60b59352ab90789964054
rodrigoprogramacion <rodrigo.programacion@gmail.com>
2022-07-20 15:02:16 -0300

    v.01

---
+++ app.py
@@ -0,0 +1,62 @@
+
+import requests
+import config
+from flask import Flask, request, json, jsonify
+from binance.cm_futures import CMFutures
+from datetime import datetime
+
+
+
+
+def ObtenerFechaServer() -> int:
+    endpoint= "https://testnet.binancefuture.com/fapi/v1/time"
+    r = requests.get(endpoint)
+    resp = r.json()
+    return resp["serverTime"]
+
+def log(texto:str):
+    f = open("ordenes.log", "a")
+    f.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " -> " + texto + "\n")
+    f.close()
+
+
+
+app = Flask(__name__)
+
+cm_futures_client = CMFutures (key=config.API_KEY, secret=config.SECRET_KEY)
+
+def orden(side, quantity, symbol):
+    try:
+        print(f"Enviando orden... {side} - {quantity} - {symbol}")
+        orden = cm_futures_client.new_order(symbol=symbol, side=side, type="MARKET", quantity=quantity, timestamp=str(ObtenerFechaServer()))
+        print(orden)
+    except Exception as e:
+        print("a ocurrido un error - {}".format(e))
+        return False
+    return True
+
+
+
+@app.route("/alerta", methods=['POST'])
+
+def alerta():
+    
+    data = json.loads(request.data)    
+    side = data['strategy']['order_action'].upper()                                       
+    quantity = data['strategy']['order_contracts']
+    symbol = data['ticker'].upper()
+    passphrase= data['passphrase']
+    enviar_orden=orden(side,quantity,symbol)
+    
+
+    return {
+            
+            "Conexion":"Exitosa",
+            "Mensaje": data,
+           
+            }
+
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=80, debug=True)
